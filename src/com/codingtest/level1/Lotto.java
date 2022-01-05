8bcda9c386960c82ad388f6e38cfe6e4ad5dcc6c
underdarks <rkdrl45617@gmail.com>
2022-01-05 14:45:19 +0900

    리팩토링 1차 적용
    코드 개선 및 품질 향상

--- src/com/codingtest/level1/Lotto.java
+++ src/com/codingtest/level1/Lotto.java
@@ -1,75 +1,46 @@
 package com.codingtest.level1;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 
-import java.util.List;
 import java.util.stream.IntStream;
 
 public class Lotto {
-        public int[] solution(int[] lottos, int[] win_nums) {
-            List<Integer> result=new ArrayList<Integer>();
+        public Map<Integer,Integer> setRank(){
+            //당첨 갯수,순위 Map 구현
+            Map<Integer,Integer> score=new HashMap<>();
 
-            Integer[] answer = {};
+            score.put(6,1);
+            score.put(5,2);
+            score.put(4,3);
+            score.put(3,4);
+            score.put(2,5);
+            score.put(1,6);
+            score.put(0,6);
+
+            return score;
+        }
+
+        public int[] solution(int[] lottos, int[] win_nums) {
+            int []answer=new int[2];
             int zero_count=0;   //숫자가 0인 갯수
             int totalCount=0;   //복권 로또 일치 개수
+            Map<Integer,Integer> score=setRank();
 
             //시간복잡도 O(n^2)
             for (int i : lottos) {
                 //0인 숫자 찾기
-                if(i == 0)
-                    zero_count++;
+                if(i == 0)  zero_count++;
 
                 //로또 숫자 확인
                 else
                     if (Arrays.stream(win_nums).anyMatch(val -> val == i) == true)
                         totalCount++;
-
             }
 
-            //최고 등수
-            switch (totalCount+zero_count){
-                case 6: result.add(1);
-                    break;
+            answer[0]=score.get(totalCount+zero_count); //최고점수
+            answer[1]=score.get(totalCount);            //최저점수
 
-                case 5: result.add(2);
-                    break;
-
-                case 4: result.add(3);
-                    break;
-
-                case 3: result.add(4);
-                    break;
-
-                case 2: result.add(5);
-                    break;
-
-                default: result.add(6);
-                    break;
-            }
-
-            //최저점수
-            switch (totalCount){
-                case 6: result.add(1);
-                    break;
-
-                case 5: result.add(2);
-                    break;
-
-                case 4: result.add(3);
-                    break;
-
-                case 3: result.add(4);
-                    break;
-
-                case 2: result.add(5);
-                    break;
-
-                default: result.add(6);
-                    break;
-            }
-
-            return result.stream().mapToInt(i->i).toArray();
+            return answer;
         }
 
 
