3d2d66b342cf44596d736a2e32d9aa6135fe8221
underdarks <rkdrl45617@gmail.com>
2022-01-06 16:17:29 +0900

    리팩토링 후
    이진 탐색 적용

--- src/com/codingtest/level1/NoNumAdd.java
+++ src/com/codingtest/level1/NoNumAdd.java
@@ -3,16 +3,43 @@ package com.codingtest.level1;
 import java.util.Arrays;
 
 public class NoNumAdd {
+
+    public boolean binarySearch(int []nums,int findNum)
+    {
+        int startIndex=0;
+        int finalIndex=nums.length-1;
+
+        //이진탐색
+        while(startIndex <= finalIndex){
+            int mid=(startIndex+finalIndex)/2;
+
+            if(nums[mid] == findNum)        //해당 숫자이면
+                return true;
+
+            else if(nums[mid] > findNum)    //중간값이 해당 숫자보다 크면
+                finalIndex=mid-1;
+
+            else                            //중간값이 해당 숫자보다 작으면
+                startIndex=mid+1;
+
+        }
+
+        return false;
+    }
+
+
     public int solution(int[] numbers) {
         int answer = 0;
-        Arrays.sort(numbers);
+        Arrays.sort(numbers);   //이진 탐색을 위한 오름차순 정렬
 
         for(int i=1;i<10;i++) {
-            int j=i;
-            if (!Arrays.stream(numbers).anyMatch(val -> val == j)){
+            if(!binarySearch(numbers,i))
                 answer+=i;
+
+//            int j=i;
+//            if (!Arrays.stream(numbers).anyMatch(val -> val == j)){
+//                answer+=i;
             }
-        }
 
         return answer;
     }
@@ -23,6 +50,4 @@ public class NoNumAdd {
         System.out.println(noNumAdd.solution(numbers));
 
     }
-
-
 }
