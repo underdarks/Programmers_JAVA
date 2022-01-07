8b578757446cd9fb2f925476217d0fd6597f0cca
underdarks <rkdrl45617@gmail.com>
2022-01-07 16:50:12 +0900

    가운데 글자 가져오기

---
+++ src/com/codingtest/level1/MidString.java
@@ -0,0 +1,17 @@
+package com.codingtest.level1;
+
+public class MidString {
+    public String solution(String s) {
+        String answer = "";
+        int len=s.length();
+        int index=len/2;
+        if (len % 2 == 0) {   //짝수이면
+            answer = s.substring(index-1,index+1);
+        }
+
+        else
+            answer=s.substring(index,index+1);
+
+        return answer;
+    }
+}
