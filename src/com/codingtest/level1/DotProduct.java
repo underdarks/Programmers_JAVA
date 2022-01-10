fd9b6c7c7e255a9888bffad49defb930ed78196d
underdarks <rkdrl45617@gmail.com>
2022-01-10 17:25:18 +0900

    실패율

---
+++ src/com/codingtest/level1/DotProduct.java
@@ -0,0 +1,11 @@
+package com.codingtest.level1;
+
+public class DotProduct{
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        for(int i=0;i<a.length;i++)
+            answer+=a[i]*b[i];
+
+        return answer;
+    }
+}
---
+++ src/com/codingtest/level1/FailRate.java
@@ -0,0 +1,69 @@
+package com.codingtest.level1;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class FailRate {
+
+    public int[] descendingSort(int N,HashMap<Double, ArrayList<Integer>> map)
+    {
+        int []answer=new int[N];
+        int i=0;
+
+        ArrayList<Double> keys = new ArrayList<>(map.keySet());
+        keys.sort(Collections.reverseOrder());
+        for (Double key : keys) {
+            ArrayList<Integer> list=map.get(key);
+            for (Integer value : list) {
+                answer[i++]=value+1;
+            }
+        }
+
+        return answer;
+    }
+
+
+    public int[] solution(int N, int[] stages) {
+        double []answer = new double[N];  //스테이지 개수 만큼 생성
+        double beforeLoserCount=0;
+        double loserCount =0;                //스테이지 실패자
+        double totalChallenger=stages.length;  //총 도전자
+        HashMap<Double, ArrayList<Integer>> map=new HashMap<Double, ArrayList<Integer>>();
+
+        for(int i=0;i<N;i++){
+            loserCount =0;
+            for(int j=0;j< stages.length;j++) {
+                if (stages[j] < i + 2)    //스테이지를 클리어 못했으면
+                    loserCount++;
+            }
+
+            double key=((loserCount-beforeLoserCount) / (totalChallenger-beforeLoserCount));
+            ArrayList<Integer> list=new ArrayList<>();
+            if(map.containsKey(key)){ //중복값이 있으면
+                list=map.get(key);
+                list.add(i);
+                Collections.sort(list);
+            }
+            else{
+                list.add(i);
+            }
+
+            map.put(key,list);
+            beforeLoserCount=loserCount;
+        }
+
+        return descendingSort(N,map);
+    }
+
+    public static void main(String[] args) {
+        int []stages={2, 1, 2, 6, 2, 4, 3, 3};
+        FailRate f=new FailRate();
+        int []result=f.solution(5,stages);
+        for (int i : result) {
+            System.out.println("i = " + i);
+        }
+    }
+
+}
