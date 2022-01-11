c0f9bfae0921388920c9e18d91c9ad512f70d3ae
underdarks <rkdrl45617@gmail.com>
2022-01-11 15:34:07 +0900

    같은 숫자는 싫어
    리팩토링 전

--- src/com/codingtest/level1/HateSameNumber.java
+++ src/com/codingtest/level1/HateSameNumber.java
@@ -1,5 +1,32 @@
 package com.codingtest.level1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //같은 숫자는 싫어
 public class HateSameNumber {
+    public int[] solution(int []arr) {
+        int[] answer = {};
+        int history=-1;
+        List<Integer> list=new ArrayList<>();
+
+        if(arr.length > 0)
+            history=arr[0];
+
+        for(int i=1;i<arr.length;i++){
+            if(history != arr[i]) {
+                list.add(history);
+                history = arr[i];
+            }
+        }
+        list.add(arr[arr.length-1]);
+
+        return list.stream().mapToInt(Integer::intValue).toArray();
+    }
+
+    public static void main(String[] args) {
+        int []arr={5, 3, 2, 1, 3, 4};
+        HateSameNumber h=new HateSameNumber();
+        h.solution(arr);
+    }
 }
