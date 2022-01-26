b3299d35a206313e592d4da2379086b40276f711
underdarks <rkdrl45617@gmail.com>
2022-01-26 15:12:42 +0900

    n! 팩토리얼

---
+++ src/com/codingtest/codeup/recursion/BinaryConversion.java
@@ -0,0 +1,5 @@
+package com.codingtest.codeup.recursion;
+
+//1920 : (재귀함수) 2진수 변환
+public class BinaryConversion {
+}
--- src/com/codingtest/codeup/recursion/factorial.java
+++ src/com/codingtest/codeup/recursion/factorial.java
@@ -1,4 +1,22 @@
 package com.codingtest.codeup.recursion;
 
+import java.util.Scanner;
+
+//1912 : (재귀함수) 팩토리얼 계산
 public class factorial {
+
+    public static int getFactorial(int n){
+        if(n<=2)
+            return n;
+
+        return getFactorial(n-1)*n;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+
+        System.out.println(getFactorial(n));
+    }
+
 }
