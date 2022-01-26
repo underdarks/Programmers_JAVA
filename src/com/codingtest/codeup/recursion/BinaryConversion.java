f644c21fd5066ef1dbdab011fad892015ab4f101
underdarks <rkdrl45617@gmail.com>
2022-01-26 15:45:52 +0900

    2진수 변환

--- src/com/codingtest/codeup/recursion/BinaryConversion.java
+++ src/com/codingtest/codeup/recursion/BinaryConversion.java
@@ -1,5 +1,25 @@
 package com.codingtest.codeup.recursion;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 //1920 : (재귀함수) 2진수 변환
 public class BinaryConversion {
+
+    public static void getBinary(int n,StringBuilder str){
+        if(n>=0){
+            getBinary(n/2 ==0 ? -1 : n/2, str);
+            str.append(n%2);
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n = sc.nextInt();
+        StringBuilder str=new StringBuilder();
+
+        getBinary(n,str);
+        System.out.println(str.toString());
+    }
 }
