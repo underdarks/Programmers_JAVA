cdd01180b96f9bb6af1ce51183ce74cbd47bbf27
underdarks <rkdrl45617@gmail.com>
2022-02-22 08:56:07 +0900

    기지국 새로풀기

--- src/com/codingtest/programmers_school/week1/BaseStation.java
+++ src/com/codingtest/programmers_school/week1/BaseStation.java
@@ -5,47 +5,31 @@ import java.util.stream.IntStream;
 
 public class BaseStation {
 
-//    public List<Integer> doCheck(int[] stations, int w, int length){
-//        List<Integer> list=new ArrayList<>();
-//        int count=0;
-//
-//        for (int station : stations) {
-//
-//        }
-//
-//        for (int j = 0; j < check.length; j++) {
-//            if(!check[j])    //통신안되면
-//                count++;
-//
-//            else {
-//                list.add(count);
-//                count=0;
-//            }
-//        }
-//
-//        if(count > 0)
-//            list.add(count);
-//
-//        return list;
-//    }
+    public void doCheck(boolean[] check, int w, int station){
+        int lastIndex=station+w >= check.length ? check.length-1: station+w;    //맨끝 인덱스만 체크
+        check[lastIndex]=true;
+    }
 
     public int solution(int n, int[] stations, int w) {
-        int answer=0;
-        int now=0;  // 1<=n<=10,000
-        int idx=0;
-        int distance=2*w+1;
+        int answer = 0;
+        int index=0;
+        boolean []check=new boolean[n];
 
-        while (now < n){
-            if (idx < stations.length && now + w >= stations[idx] - 1)    //기지국 이면
-            {
-                now = stations[idx] + w;
-                idx++;
-            }
+        for (int i = 0; i < check.length; i++) {
+            //통신 여부 확인
+            if(!check[i]) {
+                if(index < stations.length && stations[index] -1 <= i+w)   //기지국이 이미 설치되어 있으면
+                {
+                    int station = stations[index++]-1;
+                    doCheck(check,w,station);
+                    i=station+w;
+                }
 
-            //기지국이 아니면
-            else {
-                answer++;   //기지국 설치
-                now += distance;
+                else {  //기지국 미설치이면
+                    doCheck(check, w, i + w);
+                    i += w * 2;
+                    answer++;
+                }
             }
         }
 
